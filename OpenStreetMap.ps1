21db8ef3e2cc6b00b561383f2f59536c51d2675f
Christian Ehm <christian@myoggradio.org>
2020-12-16 12:51:57 +0100

    Startskript liest nach Ausfuehrung von Python noch eine Eingabe, damit Fehler

--- OpenStreetMap.ps1
+++ OpenStreetMap.ps1
@@ -5,3 +5,4 @@ $Dir = $PSScriptRoot
 $py = $Dir + "\" + "run.py"
 $ini = $Dir + "\" + "run.ini"
 python $py $ini
+Read-Host -Prompt "Achte auf Fehlermeldungen"
--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 #
 python3 run.py runLinux.ini
+read -1
 exit
